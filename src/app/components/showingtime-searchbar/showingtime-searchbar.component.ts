3cd36ce7d474535f2cb0bff5099f55ec08fa4b1d
Ninoslav Zivanovic <nzivanovic@showingtime.com>
2021-01-03 11:37:14 +0100

    ShowingTime - Searchbar component
    I've created a new component that will be used to present the 'searchbar' element in the way how the received ShowingTime UI design is expecting.

---
+++ src/app/components/showingtime-searchbar/showingtime-searchbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showingtime-searchbar',
+  templateUrl: './showingtime-searchbar.component.html',
+  styleUrls: ['./showingtime-searchbar.component.scss'],
+})
+export class ShowingtimeSearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
